26467ffbd5485f8e2c056c22136d61af1c47b1b4
Georgy <andreevgeorge@yandex.ru>
2021-11-03 08:07:03 +0100

    test

--- db/seed.js
+++ db/seed.js
@@ -1,31 +1,49 @@
-// const mongoose = require("mongoose");
-// const User = require("./models/user");
-// const { dbUrl, options } = require('./config');
+const mongoose = require("mongoose");
+const User = require("./models/user");
+const { dbUrl, options } = require('./config');
 
-// (async () => {
-//   try {
-//     console.log('Conecting...')
-//     await mongoose.connect(dbUrl, options);
-//     console.log('Mongoose connected to remote database successfully');
+(async () => {
+  try {
+    console.log('Conecting...')
+    await mongoose.connect(dbUrl, options);
+    console.log('Mongoose connected to remote database successfully');
 
-//     await Entry.deleteMany();
+    await Entry.deleteMany();
 
-//     console.log('Seeding...');
-//     await Entry.insertMany([
-//       {singer: "Илона Гольман", songTitle: "Nat King Cole - LOVE"},
-//       {singer: "Александр Степанченко", songTitle: "Лев Лещенко - Прощай"},
-//       {singer: "Гоша Бабаян", songTitle: "Frank Sinatra - My Way"},
-//       {singer: "Дмитрий Уткин", songTitle: "Филипп Киркоров - Цвет настроения синий"},
-//       {singer: "Евгений Маслович", songTitle: "Jamiroquai - Space Cowboy"},
-//       {singer: "Игорь Никифоров", songTitle: "Слот - Платно"},
-//       {singer: "Даниил Капустин", songTitle: "Heart Of A Coward - Miscreation"},
-//     ]);
-//   }
-//   catch (e) {
-//     console.error('Error:', e.message)
-//   }
-//   finally {
-//     await mongoose.connection.close()
-//     console.log('DB closed');
-//   }
-// })()
+    console.log('Seeding...');
+
+    await Tag.insertMany([
+      {tagname: "JavaScript"},
+      {tagname: "React"},
+      {tagname: "Java"},
+      {tagname: "Python"},
+      {tagname: "PostgreSQL"},
+      {tagname: "Node.js"},
+      {tagname: "PHP"},
+      {tagname: "MongoDB"},
+      {tagname: "Oracle"},
+      {tagname: "Cassandra"},
+      {tagname: "React Native"},
+      {tagname: "Go"},
+      {tagname: "Ruby"},
+      {tagname: "Algorithms"},
+      {tagname: "Test-cases"},
+      {tagname: "Soft skills"},
+      {tagname: "Time management"},
+      {tagname: "Crisis management"},
+      {tagname: "HR training"},
+      {tagname: "Finance modelling"},
+      {tagname: "Data analysis"},
+      {tagname: "C++"},
+      {tagname: "AI & ML"},
+      {tagname: "Databases"},
+    ]);
+  }
+  catch (e) {
+    console.error('Error:', e.message)
+  }
+  finally {
+    await mongoose.connection.close()
+    console.log('DB closed');
+  }
+})()
