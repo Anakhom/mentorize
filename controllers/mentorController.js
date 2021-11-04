c1e6872472d1f6cec564bbe3d84f8d7ca00fc83a
Egor <egor.bartuli@gmail.com>
2021-11-04 10:48:35 +0300

    all mentors done

--- controllers/mentorController.js
+++ controllers/mentorController.js
@@ -1,17 +1,15 @@
 const Mentor = require('../db/models/mentor');
-it 
+const Tag = require('../db/models/tag');
+
 exports.renderMentors = async (req, res) => {
   let mentors;
   try {
-    mentors = await Mentor.findOne().populate('stack');
-    console.log(mentors);
+    mentors = await Mentor.find().populate('stack');
   } catch (error) {
-    console.log(error.message);
-    // return res.render('error', {
-    //   message: 'Не удалось получить записи из базы данных.',
-    //   error: {},
-
+    return res.render('error', {
+      message: 'Не удалось получить записи из базы данных.',
+      error: {},
+    });
   }
-
   return res.render('mentors', { mentors });
 };
--- db/models/tag.js
+++ db/models/tag.js
@@ -1,16 +1,15 @@
 const mongoose = require('mongoose');
 const { Schema } = require('mongoose');
 
-
 const tagSchema = new mongoose.Schema({
   tagname: String,
-  // mentor_id: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: 'mentor',
-  //   required: false,
-  // },
+  mentor_id: {
+    type: Schema.Types.ObjectId,
+    ref: 'mentor',
+    required: false,
+  },
   createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }
+  updatedAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('Tag', tagSchema);
