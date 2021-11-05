849e3b709380f33e30284c4da54e69391073650f
Georgy <andreevgeorge@yandex.ru>
2021-11-05 11:11:37 +0100

    test6

--- db/config.js
+++ db/config.js
@@ -1,7 +1,9 @@
-const dbUrl = 'mongodb://localhost:27017/mentorize2';
+require('dotenv').config();
+
+const dbUrl = process.env.DB_URL_PROD;
 const options = {
   useUnifiedTopology: true,
-  useNewUrlParser: true
+  useNewUrlParser: true,
 };
 
 module.exports = { dbUrl, options };
