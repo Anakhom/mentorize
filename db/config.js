30e7dc0b772a7aa7b71ef39c16bb159ede64aa33
Anastasia K <anastasia.khomchenko@gmail.com>
2021-11-05 10:51:44 +0300

    heroku and cloud DB

--- db/config.js
+++ db/config.js
@@ -1,4 +1,6 @@
-const dbUrl = 'mongodb://localhost:27017/mentorize2';
+require('dotenv').config();
+
+const dbUrl = process.env.DB_URL_PROD;
 const options = {
   useUnifiedTopology: true,
   useNewUrlParser: true
